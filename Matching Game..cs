72d0a877f601f159256e6b5a254f0fabe350d8f9
kamalesh <kamalikamalesh0401@gmail.com>
2025-04-10 19:40:47 +0530

    Add project files.

---
+++ Matching Game..cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFMatchingGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!",     // Exclamation mark - speaker
+            "N", "N",     // Flag
+            "k", "k",     // Airplane
+            "y", "y",     // Checkmark
+            "b", "b",     // Star
+            "v", "v",     // Key
+            "w", "w",     // Lock
+            "z", "z",     // Heart
+            "d", "d",     // Clock
+            "t", "t",     // Mail
+            "f", "f",     // Note
+            "h", "h",     // Pen
+            "j", "j",     // Globe
+            "l", "l",     // Eye
+            "p", "p"      // Thumbs up
+        };
+
+        Label firstClicked = null;
+        Label secondClicked = null;
+        private int elapsedSeconds = 0;
+        private int elapsedMinutes = 0;
+        private int elapsedHours = 0;
+        private int animationStep = 0;
+
+
+        private void ResetValues()
+        {
+            //firstClicked = null;
+            //secondClicked = null;
+            elapsedSeconds = 0;
+            elapsedMinutes = 0;
+            elapsedHours = 0;
+            animationStep = 0;
+            icons = new List<string>()
+            {
+            "!", "!","N", "N","k", "k","y", "y","b", "b","v", "v","w", "w","z", "z","d", "d","t", "t","f", "f","h", "h","j", "j","l", "l","p", "p"
+            };
+            AssignIconsToSquares();
+        }
+
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+            timer2.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                // ignore the click when the label already revealed
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                // If firstClicked is null,
+                // so set firstClicked to the label that the player 
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    HighlightMatch(firstClicked, secondClicked);
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+                timer1.Start();
+            }
+        }
+        private void CheckForWinner()
+        {
+            // checking each one to see if its icon is matched
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+            timer2.Stop();
+            string timeDisplay = string.Format("{0:00}:{1:00}:{2:00}", elapsedHours, elapsedMinutes, elapsedSeconds);
+            MessageBox.Show("You matched all the icons! " + timeDisplay + "", "Congratulations");
+            //Close();
+            ResetValues();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            //HighlightMisMatch(firstClicked, secondClicked);
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            // Reset firstClicked and secondClicked 
+            // so the next time a label is
+            // clicked, the program knows it's the first click
+            firstClicked = null;
+            secondClicked = null;
+
+            CheckForWinner();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            elapsedSeconds++;
+            if (elapsedSeconds == 60)
+            {
+                elapsedMinutes++;
+                elapsedSeconds = 0;
+            }
+            if (elapsedMinutes == 60)
+            {
+                elapsedHours++;
+                elapsedMinutes = 0;
+            }
+
+            // Format the time as HH:MM:SS
+            string timeDisplay = string.Format("{0:00}:{1:00}:{2:00}", elapsedHours, elapsedMinutes, elapsedSeconds);
+
+            this.label17.Text = "Timer :" + timeDisplay;
+
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            foreach (Control c in tableLayoutPanel1.Controls)
+            {
+                float newFontSize = this.Width / 30f; // Adjust this value as needed
+                c.Font = new Font("Webdings", newFontSize, FontStyle.Bold);
+            }
+        }
+        private async void HighlightMatch(Label card1, Label card2)
+        {
+            var temp = card1.BackColor;
+            card1.BackColor = Color.LimeGreen;
+            card2.BackColor = Color.LimeGreen;
+            await Task.Delay(300);
+            card1.BackColor = temp;
+            card2.BackColor = temp;
+        }
+        private async void HighlightMisMatch(Label card1, Label card2)
+        {
+            var temp = card1.BackColor;
+            card1.BackColor = Color.DarkRed;
+            card2.BackColor = Color.DarkRed;
+            await Task.Delay(300);
+            card1.ForeColor = Color.Black;
+            card2.ForeColor = Color.Black;
+            card1.BackColor = temp;
+            card2.BackColor = temp;
+        }
+
+        //private async void HighlightMatch(PictureBox card1, PictureBox card2)
+        //{
+        //    card1.BackColor = Color.LimeGreen;
+        //    card2.BackColor = Color.LimeGreen;
+        //    await Task.Delay(300);
+        //    card1.BackColor = Color.White;
+        //    card2.BackColor = Color.White;
+        //}
+    }
+
+}
